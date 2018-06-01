7ac8cb173bcf8bad5031ec15e16d62102128a587
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-05-31 21:39:20 -0400

    Create donate.js

---
+++ commands/donate.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args) => {
+    message.channel.send(`Hey there, Do want to donate for \`Logan\`? This is the link https://www.paypal.com/pools/c/826YOnBmFY & https://www.patreon.com/loganbot (if you want to), but, Why would you donate us?\n\
+    **1.** I'm doing it for free and would love for Support! \n\
+    **2.** Logan needs esources and I need to pay for it..\n\
+    **3.** I'm working on this bot everyday and putting my daily affort in it!\n\
+    **Thank you if you decided to become a partner Also Dm me for your role!!** :heart:`);
+}
